91ccaeedb594df0eb6dc977d4e7636179626302f
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2021-09-12 00:19:05 +0900

    Implement mv embedding

--- rainfallflow/prog.r
+++ rainfallflow/prog.r
@@ -86,11 +86,11 @@ predict.chaotic.rnn = function(emb, train.size=0.7){
 
 	model <- trainr(Y=trainY,
 		X=trainX,
-		learningrate   = 0.07,
+		learningrate   = 0.05,
 		# learningrate   = 0.04,   # For m=8, d=17
 		hidden_dim     = 18,
 		batch_size     = 100,
-		numepochs      = 200,
+		numepochs      = 1000,
 		momentum       = 0.1,
 		use_bias       = TRUE,
 		seq_to_seq_unsync=TRUE);
@@ -106,12 +106,48 @@ predict.chaotic.rnn = function(emb, train.size=0.7){
 	# predict
 	preds <- predictr(model, testX);
 
-	plot(testY, type="l", xlab="Time", ylab="Sunspots", lwd=3);
+	plot(testY, pch=19, xlab="Time", ylab="Rainflow");
 	lines(preds[,1], col=2, lwd=3, lty=3);
 	rmse = report(testY, preds[,1]);
 	savePlot("chaotic-rnn.png");
 
 	return(rmse);
+}
+
+
+rainflow.mv.embedd = function(
+	Q, US1, US2, US3, RG1, RG2, RG3, RG4, RG5,
+	lags.Q, lags.US1, lags.US2, lags.RG1, lags.RG2, lags.RG3, lags.RG4, lags.RG5
+){
+	emb.Q = embedd(Q, lags=lags.Q);
+	emb.US1 = embedd(US1, lags=lags.US1);
+	emb.US2 = embedd(US2, lags=lags.US2);
+	emb.US3 = embedd(US3, lags=lags.US3);
+	emb.RG1 = embedd(RG1, lags=lags.RG1);
+	emb.RG2 = embedd(RG2, lags=lags.RG2);
+	emb.RG3 = embedd(RG3, lags=lags.RG3);
+	emb.RG4 = embedd(RG4, lags=lags.RG4);
+	emb.RG5 = embedd(RG5, lags=lags.RG5);
+
+	emb.all = list(emb.Q,emb.US1,emb.US2,emb.US3,emb.RG1,emb.RG2,emb.RG3,emb.RG4,emb.RG5);
+
+	min.acc = 2**32;
+	for(i in length(emb.all)){
+		local.emb = emb.all[[i]];
+		
+		min.acc = min(min.acc, nrow(local.emb));
+	}
+
+	for(i in length(emb.all)){
+		local.emb = emb.all[[i]];
+
+		len = nrow(local.emb);
+
+		emb.all[[i]] = local.emb[(len-min.acc + 1):len,];
+	}
+
+	full.emb = cbind(emb.all[[9]], emb.all[[8]], emb.all[[7]], emb.all[[6]], emb.all[[5]], emb.all[[4]], emb.all[[3]], emb.all[[2]], emb.all[[1]]);
+	return(full.emb);
 }
 
 graphics.off();
@@ -121,7 +157,7 @@ df1 = read.csv("Train1.csv")
 df2 = read.csv("Train2.csv")
 df3 = read.csv("Test.csv")
 
-test.df = rbind(df1, df2);
+test.df = rbind(df1, df2, df3);
 
 series = test.df$Q;
 cat("Normalization factor: ", max(abs(series)), "\n");
@@ -130,14 +166,14 @@ series = series / max(abs(series));
 # Take care here
 # lags = c(0,34,34*2,34*3) will feed 34 first to the RNN, and 34*3 lastly (more near the RNN output)
 
-emb = embedd(series, lags=c(0,c(5,4,3,1)));
+emb = embedd(series, lags=c(4,3,2,1));
 #emb = embedd(series, lags=c(0,3,6));
 
-rmse1 = predict.dwnn(emb);
-#rmse2 = predict.chaotic.rnn(emb);
+#rmse1 = predict.dwnn(emb);
+rmse2 = predict.chaotic.rnn(emb);
 #print(c(rmse1, rmse2));
 
-stop("Oh shit.");
+
 
 #A = emb[,1:(ncol(emb - 1))];
 #B = emb[,ncol(emb)];
